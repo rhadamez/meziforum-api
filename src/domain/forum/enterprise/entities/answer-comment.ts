7244fd7da2ad9625918bea7623bbd3ce00d888aa
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-06-18 20:27:11 -0300

    feat: add abstract Comment class

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,16 +1,12 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityID
+export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
+export class AnswerComment extends Comment<AnswerCommentProps> {
 
 	static create(props: Optional<AnswerCommentProps, 'createdAt'>, id?: UniqueEntityID) {
 		const answerComment = new AnswerComment({
@@ -21,37 +17,7 @@ export class AnswerComment extends Entity<AnswerCommentProps> {
 		return answerComment
 	}
 
-	get content() {
-		return this.props.content
-	}
-
-	set content(content: string) {
-		this.props.content = content
-		this.touch()
-	}
-
-	get authorId() {
-		return this.props.authorId
-	}
-
 	get answerId() {
 		return this.props.answerId
 	}
-
-	get createdAt() {
-		return this.props.createdAt
-	}
-
-	get updatedAt() {
-		return this.props.updatedAt
-	}
-
-	get excerpt() {
-		return this.content.substring(0, 120).trimEnd().concat('...')
-	}
-
-	private touch() {
-		this.props.updatedAt = new Date()
-	}
-
 }
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,16 +1,12 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
+export class QuestionComment extends Comment<QuestionCommentProps> {
 
 	static create(props: Optional<QuestionCommentProps, 'createdAt'>, id?: UniqueEntityID) {
 		const questionComment = new QuestionComment({
@@ -21,37 +17,7 @@ export class QuestionComment extends Entity<QuestionCommentProps> {
 		return questionComment
 	}
 
-	get content() {
-		return this.props.content
-	}
-
-	set content(content: string) {
-		this.props.content = content
-		this.touch()
-	}
-
-	get authorId() {
-		return this.props.authorId
-	}
-
 	get questionId() {
 		return this.props.questionId
 	}
-
-	get createdAt() {
-		return this.props.createdAt
-	}
-
-	get updatedAt() {
-		return this.props.updatedAt
-	}
-
-	get excerpt() {
-		return this.content.substring(0, 120).trimEnd().concat('...')
-	}
-
-	private touch() {
-		this.props.updatedAt = new Date()
-	}
-
 }
