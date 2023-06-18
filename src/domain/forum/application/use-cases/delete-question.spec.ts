b5b1effb538d476f8bc39740a15887253af4d400
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-06-18 16:13:01 -0300

    feat: allow only authors to delete its own questions

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -13,12 +13,26 @@ describe('Delete Question', () => {
 	})
 
 	test('should be able to delete a question', async () => {
-		const newQuestion = makeQuestion({}, new UniqueEntityID('question-1'))
+		const newQuestion = makeQuestion({
+			authorId: new UniqueEntityID('author-1')
+		}, new UniqueEntityID('question-1'))
 
-		questionsRepository.create(newQuestion)
+		await questionsRepository.create(newQuestion)
 
-		await sut.execute({ questionId: 'question-1' })
+		await sut.execute({ authorId: 'author-1', questionId: 'question-1' })
 
 		expect(questionsRepository.items).toHaveLength(0)
+	})
+
+	test('should not be able to delete a question from a non author user', async () => {
+		const newQuestion = makeQuestion({
+			authorId: new UniqueEntityID('author-1')
+		}, new UniqueEntityID('question-1'))
+
+		await questionsRepository.create(newQuestion)
+
+		expect(() => {
+			return sut.execute({ authorId: 'author-2', questionId: 'question-1' })
+		}).rejects.toBeInstanceOf(Error)
 	})
 })
--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -1,6 +1,7 @@
 import { QuestionsRepository } from '../repositories/question-repository'
 
 interface DeleteQuestionUseCaseRequest {
+	authorId: string
 	questionId: string
 }
 
@@ -10,10 +11,12 @@ export class DeleteQuestionUseCase {
     private questionsRepository: QuestionsRepository
 	) { }
 
-	async execute({ questionId }: DeleteQuestionUseCaseRequest): Promise<void> {
+	async execute({ authorId, questionId }: DeleteQuestionUseCaseRequest): Promise<void> {
 		const question = await this.questionsRepository.findById(questionId)
 
 		if(!question) throw new Error('Question not found.')
+
+		if(authorId !== question.authorId.toString()) throw new Error('Not allowed.')
 
 		await this.questionsRepository.delete(question)
 	}
