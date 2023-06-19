1985919f1a5b5aeff2bf1a42c1608f6e67c70b6b
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-06-18 21:54:42 -0300

    feat: add findById and delete in the comments repository

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -8,4 +8,16 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
 		this.items.push(answerComment)
 	}
 
+	async findById(id: string): Promise<AnswerComment | null> {
+		const answercomment = this.items.find(item => item.id.toString() === id)
+		if(!answercomment) return null
+
+		return answercomment
+	}
+
+	async delete(answercomment: AnswerComment): Promise<void> {
+		const answercommentIndex = this.items.findIndex(item => item.id === answercomment.id)
+		this.items.splice(answercommentIndex, 1)
+	}
+
 }
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -8,4 +8,16 @@ export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepos
 		this.items.push(questionComment)
 	}
 
+	async findById(id: string): Promise<QuestionComment | null> {
+		const questioncomment = this.items.find(item => item.id.toString() === id)
+		if(!questioncomment) return null
+
+		return questioncomment
+	}
+
+	async delete(questioncomment: QuestionComment): Promise<void> {
+		const questioncommentIndex = this.items.findIndex(item => item.id === questioncomment.id)
+		this.items.splice(questioncommentIndex, 1)
+	}
+
 }
