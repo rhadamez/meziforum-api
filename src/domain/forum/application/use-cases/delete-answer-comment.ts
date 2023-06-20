41d97f95c646aaf3e5df0c48e0d537abfc590849
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-06-20 01:51:23 -0300

    feat: add error handling classes

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { Either, left, right } from '@/core/either'
 import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
 
 interface DeleteAnswerCommentUseCaseRequest {
@@ -5,17 +6,21 @@ interface DeleteAnswerCommentUseCaseRequest {
 	answerCommentId: string
 }
 
+type DeleteAnswerCommentUseCaseResponse = Either<string, object>
+
 export class DeleteAnswerComment {
 	constructor(
     private answerCommentsRepository: AnswerCommentsRepository
 	) { }
 
-	async execute({ authorId, answerCommentId }: DeleteAnswerCommentUseCaseRequest): Promise<void> {
+	async execute({ authorId, answerCommentId }: DeleteAnswerCommentUseCaseRequest): Promise<DeleteAnswerCommentUseCaseResponse> {
 		const answerComment = await this.answerCommentsRepository.findById(answerCommentId)
-		if(!answerComment) throw new Error('Comment not found on this answer.')
+		if(!answerComment) return left('Comment not found on this answer.')
 
-		if(authorId !== answerComment.authorId.toString()) throw new Error('Not allowed.')
+		if(authorId !== answerComment.authorId.toString()) return left('Not allowed.')
 
 		await this.answerCommentsRepository.delete(answerComment)
+
+		return right({})
 	}
 }
